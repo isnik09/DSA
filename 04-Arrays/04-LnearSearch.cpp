875e8e9c60bfa22fcc659aec2c063a28d1260f64
NIK Nerkar <nik.nerkar@outlook.com>
2023-02-03 18:33:59 +0530

    Linear search

---
+++ 04-Arrays/04-LnearSearch.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    int toSearch, rangeOfArray;
+
+    cout << "Enter how long you want your array to be: " << endl;
+    cin >> rangeOfArray;
+    int A[rangeOfArray];
+    for (int &x : A)
+    {
+        cout << "Enter a number: " << endl;
+        cin >> x;
+    }
+
+    cout << "What number you want to find? " << endl;
+    cin>>toSearch;
+    for (int x : A)
+    {
+        if (toSearch == x)
+        {
+            cout << "Found" << endl;
+            return 0;
+        }
+    }
+    cout << "Not found" << endl;
+    return 0;
+}
--- 04-Arrays/tempCodeRunnerFile.cpp
+++ 04-Arrays/tempCodeRunnerFile.cpp
@@ -1,4 +1,1 @@
-   // for (int i = 0; i < 5; i++)
-    // {   
-    //     cout << A[i] << endl;
-    // }
+i
