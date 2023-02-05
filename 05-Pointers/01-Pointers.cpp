f702b63e0fe80a89072fb1062b8561a6c4fe8ae2
NIK Nerkar <iamp1.nik@outlook.com>
2023-02-05 16:00:01 +0530

    pointers

--- 05-Pointers/01-Pointers.cpp
+++ 05-Pointers/01-Pointers.cpp
@@ -3,7 +3,14 @@ using namespace std;
 int main()
 {
 
-    cout << "Pointers " << endl;
+    int a = 10;
+    int *p = &a;
+
+    cout << a << endl;
+    cout << &a << endl;
+    cout << p << endl;
+    cout << &p << endl;
+    cout << *p << endl;
 
     return 0;
 }
