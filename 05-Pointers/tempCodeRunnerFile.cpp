03d291403626ad2bee8309cab79485ed40e45f72
NIK Nerkar <iamp1.nik@outlook.com>
2023-02-08 22:15:49 +0530

    cleaned files

--- 05-Pointers/tempCodeRunnerFile.cpp
+++
@@ -1,9 +0,0 @@
-    int *p = new int[5];
-    p[0] = 12;
-    p[1] = 13;
-    cout << p[1] << endl;
-    delete[] p;
-    int A[10];
-    cout << "Size of an array with 10 int elements is " << sizeof A << endl;
-    //  p=NULL;  used in OLD CPP
-    p = nullptr;
