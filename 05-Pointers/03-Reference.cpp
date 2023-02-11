37c8b05909cfdfaf542b198163ff1f57d68b8582
NIK Nerkar <iamp1.nik@outlook.com>
2023-02-11 19:12:38 +0530

    pointers reference

---
+++ 05-Pointers/03-Reference.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x = 10;
+    int &b = x;
+    x += 10;
+    cout << x << " " << b << endl;
+    return 0;
+}
+
+
+// IT WILL PRINT 20 and 20 as b 
+// points to x and it's value changed...
